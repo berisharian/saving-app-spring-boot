a664ee77767f864ab90e322945af6544d86aecde
Arian Berisha <105926060+berisharian@users.noreply.github.com>
2024-08-09 18:44:35 +0200

    Initializing project

---
+++ src/main/java/finki/ukim/mk/savingapp/SavingAppApplication.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.savingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/finki/ukim/mk/savingapp/SavingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.savingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
